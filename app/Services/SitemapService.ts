c27ea10b91d0797063b56a189ca93c20f8d696a0
Dolu <dolu89@gmail.com>
2021-09-05 17:08:05 +0200

    Refactor redis to BipService

--- app/Services/SitemapService.ts
+++ app/Services/SitemapService.ts
@@ -1,6 +1,6 @@
 import { SitemapManager } from 'sitemap-manager'
 import Env from '@ioc:Adonis/Core/Env'
-import Redis from '@ioc:Adonis/Addons/Redis'
+import BipService from './BipService'
 
 class SitemapService {
   private static sitemap = new SitemapManager({
@@ -9,27 +9,33 @@ class SitemapService {
   private routes: string[] = ['/', '/support']
 
   public async generate() {
+    let lastUpdate: Date | null = null
+
+    // Add BIPs pages
+    let bipsUrls: Url[] = []
+    const bips = await BipService.getAll()
+    for (const bip of bips) {
+      bipsUrls.push({
+        loc: bip.bip,
+        lastmod: bip.updated,
+      })
+
+      const bipUpdateDate = new Date(bip.updated)
+      if (lastUpdate === null || lastUpdate < bipUpdateDate) {
+        lastUpdate = bipUpdateDate
+      }
+    }
+    SitemapService.sitemap.addUrl('bips', bipsUrls)
+
     // Add static pages
     let pagesUrls: Url[] = []
     for (const route of this.routes) {
       pagesUrls.push({
         loc: route,
-        lastmod: '' || new Date(),
+        lastmod: lastUpdate!,
       })
     }
     SitemapService.sitemap.addUrl('pages', pagesUrls)
-
-    // Add BIPs pages
-    let bipsUrls: Url[] = []
-    const keys = await Redis.keys('bip:*')
-    for (const key of keys) {
-      const bip = await Redis.hgetall(key)
-      bipsUrls.push({
-        loc: bip.bip,
-        lastmod: bip.updated,
-      })
-    }
-    SitemapService.sitemap.addUrl('bips', bipsUrls)
 
     await SitemapService.sitemap.finish()
   }
