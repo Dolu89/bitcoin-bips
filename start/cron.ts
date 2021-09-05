957d127125519f77619c2b0696d862690c07451f
Dolu <dolu89@gmail.com>
2021-09-05 15:59:00 +0200

    Update deps

--- start/cron.ts
+++ start/cron.ts
@@ -2,7 +2,5 @@ import cron from 'node-cron'
 import UpdateBips from 'App/Services/UpdateBips'
 
 cron.schedule('0 0 * * *', async () => {
-  console.log('STARTING CRON')
   await UpdateBips.process()
-  console.log('Ending cron')
 })
