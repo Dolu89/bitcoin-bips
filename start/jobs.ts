17a57a7ec8a4573067b05cdc6cc63f67564eb3d0
Dolu <doludev@protonmail.com>
2024-01-21 15:00:03 +0100

    feat: add cron to update bips

--- start/jobs.ts
+++ start/jobs.ts
@@ -1,7 +1,20 @@
 import app from '@adonisjs/core/services/app'
 import UpdaterService from '#services/updater_service'
+import cron from 'node-cron'
 
 const service = await app.container.make(UpdaterService)
 service.updateBips().then(() => {
   console.log('Initial BIPS update done')
+}).catch((error) => {
+  console.log('Error while updating BIPS', error)
+})
+
+cron.schedule('0 0 * * *', async () => {
+  console.log('Refreshing BIPS starting...')
+  try {
+    await service.updateBips()
+    console.log('Refreshing BIPS done')
+  } catch (error) {
+    console.log('Error while refreshing BIPS', error)
+  }
 })
