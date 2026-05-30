2caa6f9bc8a9dc583aab0ad913301d096ad10b46
Dolu <doludev@protonmail.com>
2026-05-30 22:02:59 +0200

    chore(scaffold): flow tooling and project docs

--- adonisrc.ts
+++ adonisrc.ts
@@ -27,6 +27,7 @@ export default defineConfig({
     () => import('@adonisjs/core/commands'),
     () => import('@adonisjs/lucid/commands'),
     () => import('@adonisjs/session/commands'),
+    () => import('@adonisplus/flow/commands'),
   ],
 
   /*
